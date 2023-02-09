db35c15427faab98c9e35820f4dcda049a0dff59
DianaSapralieva <dsapraliievaa@gmail.com>
2023-02-09 11:39:29 +0100

    10 dictionaries are saved inside a list tha is called question_data

---
+++ data.py
@@ -0,0 +1,12 @@
+question_data = [
+{"text": "Linus Torvalds created Linux and Git.", "answer": "True"},
+{"text": "The logo for Snapchat is a Bell.", "answer": "False"},
+{"text": "RAM stands for Random Access Memory.", "answer": "True"},
+{"text": "Time on Computers is measured via the EPOX System.", "answer": "False"},
+{"text": "The Windows ME operating system was released in the year 2000.", "answer": "True"},
+{"text": "Linux was first created as an alternative to Windows XP.", "answer": "False"},
+{"text": "The Python programming language gets its name from the British comedy group &quot;Monty Python.&quot;", "answer": "True"},
+{"text": "Google was originally called 'Backrub'.", "answer": "True"},
+{"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+{"text": "A few ounces of chocolate can kill a small dog.", "answer": "True"}
+]
