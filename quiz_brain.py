d0301a6b5af62590fac37109e882fdfc0ebc7b67
DianaSapralieva <dsapraliievaa@gmail.com>
2023-02-13 10:56:07 +0100

    the quiz_brain is implemented and main is modyfied

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,11 +1,13 @@
 class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
+        self.score=0
         self.question_list = question_list
 
     def next_question(self):
-        question = self.question_list[self.question_number]
-        answer = input(question)
+        current_question = self.question_list[self.question_number]
+        # print(current_question)
+        # print(type(current_question))
         self.question_number += 1
-        return answer        
+        input(f"Q.{self.question_number}:{current_question.question} (True/False):")        
 
