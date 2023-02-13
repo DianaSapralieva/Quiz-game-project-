9b95210e0a5924ceb55211866bdf992a067ba897
DianaSapralieva <dsapraliievaa@gmail.com>
2023-02-13 09:03:37 +0100

    next question method

--- quiz_brain.py
+++ quiz_brain.py
@@ -2,3 +2,10 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
+
+    def next_question(self):
+        question = self.question_list[self.question_number]
+        answer = input(question)
+        self.question_number += 1
+        return answer        
+
