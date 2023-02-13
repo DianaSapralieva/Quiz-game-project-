1951cb4a1fec85ffaeaa6dc62e6925c159c8096a
DianaSapralieva <dsapraliievaa@gmail.com>
2023-02-13 13:21:20 +0100

    check answer is implemented

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,4 +1,5 @@
 class QuizBrain:
+
     def __init__(self, question_list):
         self.question_number = 0
         self.score=0
@@ -6,14 +7,33 @@ class QuizBrain:
 
 
     def still_has_questions(self):
-        return self.question_number < len(self.question_list)
+        return self.question_number < len(self.question_list)  
+
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer == correct_answer:
+            print("You got it right!")
+            self.score+=1 
+        else:
+            print("Wrong answer") 
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
         # print(current_question)
         # print(type(current_question))
         self.question_number += 1
-        input(f"Q.{self.question_number}:{current_question.question} (True/False):")
+        user_answer=input(f"Q.{self.question_number}:{current_question.question} (True/False):")
+        correct_answer=current_question.answer
+        self.check_answer(user_answer, correct_answer)
+        
+    
+        
+
+
+   
+           
+
+     
 
             
 
