9b21a1a6c195cdf6b352b5082f318d2c8ddfabe4
DianaSapralieva <dsapraliievaa@gmail.com>
2023-02-13 11:19:41 +0100

    the while loop for the next question is implemented

--- quiz_brain.py
+++ quiz_brain.py
@@ -4,10 +4,16 @@ class QuizBrain:
         self.score=0
         self.question_list = question_list
 
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
     def next_question(self):
         current_question = self.question_list[self.question_number]
         # print(current_question)
         # print(type(current_question))
         self.question_number += 1
-        input(f"Q.{self.question_number}:{current_question.question} (True/False):")        
+        input(f"Q.{self.question_number}:{current_question.question} (True/False):")
+
+            
 
